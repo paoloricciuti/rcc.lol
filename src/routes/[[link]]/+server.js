7a7cda1e7855ef4efeab919714a797a9a2eebbf2
Paolo Ricciuti <ricciutipaolo@gmail.com>
2023-05-10 16:06:19 +0200

    add vercel adapter remove static

--- src/routes/[[link]]/+server.js
+++ src/routes/[[link]]/+server.js
@@ -1,18 +1,9 @@
-export function entries(){
-   const links_entries=import.meta.glob("./links/*");
-   return Object.keys(links_entries).map(link => ({
-      link: link.split("/").at(-1),
-   }))
-}
-
-export const prerender = true;
-
 export const GET = async ({ params: { link } }) => {
   let url = "https://ricciuti.me";
   try {
-    url = (await import(`./links/${link}.link?raw`)).default;
+    url = (await import(`./links/${link}.link`)).default;
   } catch (e) {}
-  console.log({ url });
+  console.log("Redirecting to ", url);
   return new Response(null, {
     status: 301,
     headers: {
