7587bc7cd44bc7cfd12227046fc250be91c1318f
Paolo Ricciuti <ricciutipaolo@gmail.com>
2023-05-10 18:03:22 +0200

    Add vite plugin dynamic import

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import dynamicImport from "vite-plugin-dynamic-import";
 
-function link(){
-   return {
-      name: "vite-link-plugin",
-      transform(code, id){
-         if(id.endsWith(".link")){
-            return `export default "${code}"`;
-         }
+function link() {
+  return {
+    name: "vite-link-plugin",
+    transform(code, id) {
+      if (id.endsWith(".link")) {
+        return `export default "${code}"`;
       }
-   }
+    },
+  };
 }
 
 export default defineConfig({
-	plugins: [sveltekit(), link()]
+  plugins: [sveltekit(), link(), dynamicImport()],
 });
