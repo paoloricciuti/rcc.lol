f8cbd87c56f43f4ab680884da7ac8db3e8323b28
Paolo Ricciuti <ricciutipaolo@gmail.com>
2023-05-10 18:07:22 +0200

    Add github and twitter

--- src/routes/[[link]]/+server.js
+++ src/routes/[[link]]/+server.js
@@ -1,7 +1,7 @@
 export const GET = async ({ params: { link } }) => {
   let url = "https://ricciuti.me";
   try {
-    url = (await import(`./links/${link}.link`)).default;
+    url = (await import(`./links/${link.toLowerCase()}.link`)).default;
   } catch (e) {}
   console.log("Redirecting to ", url);
   return new Response(null, {
