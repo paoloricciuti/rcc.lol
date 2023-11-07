2c1a4d2911f3a8372019a4251a3c110cd9e07a43
paoloricciuti <ricciutipaolo@gmail.com>
2023-11-07 23:10:48 +0100

    add counters for redirects using vercel kv

--- src/routes/[[link]]/+server.js
+++ src/routes/[[link]]/+server.js
@@ -1,9 +1,12 @@
+import { kv } from "@vercel/kv";
+
 export const GET = async ({ params: { link } }) => {
   let url = "https://ricciuti.me";
   try {
     url = (await import(`./links/${link.toLowerCase()}.link`)).default;
   } catch (e) {}
   console.log("Redirecting to ", url);
+  kv.incr(url);
   return new Response(null, {
     status: 301,
     headers: {
