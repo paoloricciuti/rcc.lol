5c0d6639a993d43d70431dfc41f6d5619a4d844f
paoloricciuti <ricciutipaolo@gmail.com>
2023-11-07 23:16:19 +0100

    change redis key to link

--- src/routes/[[link]]/+server.js
+++ src/routes/[[link]]/+server.js
@@ -4,9 +4,9 @@ export const GET = async ({ params: { link } }) => {
   let url = "https://ricciuti.me";
   try {
     url = (await import(`./links/${link.toLowerCase()}.link`)).default;
+    kv.incr(link.toLowerCase());
   } catch (e) {}
   console.log("Redirecting to ", url);
-  kv.incr(url);
   return new Response(null, {
     status: 301,
     headers: {
